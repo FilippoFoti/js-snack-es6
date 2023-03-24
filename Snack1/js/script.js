be2508bae72a2e9e527c1592bb2eb2318bfaec4c
FilippoFoti <filippofoti7@gmail.com>
2023-03-24 14:54:47 +0100

    Snack1 completed

---
+++ Snack1/js/script.js
@@ -0,0 +1,38 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const bike = [
+    {
+        nome: "Bianchi",
+        peso: "1,1 Kg",
+    },
+    {
+        nome: "Atala",
+        peso: "2,3 Kg",
+    },
+    {
+        nome: "Olmo",
+        peso: "5,1 Kg",
+    },
+    {
+        nome: "Casati",
+        peso: "3,5 Kg",
+    },
+];
+
+console.log(bike);
+
+let smallBike = bike[0];
+const itemsContainer = document.querySelector(".col");
+
+for(let i = 0; i < bike.length; i++) {
+    if (bike[i].peso < smallBike.peso) {
+        smallBike = bike[i];
+    }
+};
+
+const {nome, peso} = smallBike;
+
+document.getElementById("result").innerHTML = `La bici da corsa ${nome} è quella che pesa meno con ${peso}.`;
